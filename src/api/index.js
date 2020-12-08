4595c841fec0b510cb2dd25d30071a65d7cdba49
sean-mcinturff <sean4227@live.com>
2020-12-08 08:45:23 -0500

    initial commit

---
+++ src/api/index.js
@@ -0,0 +1,40 @@
+import axios from 'axios';
+
+const url = 'https://covid19.mathdro.id/api';
+
+export const fetchData = async (country) => {
+  let changeableURL = url
+
+  if(country) {
+    changeableURL = `${url}/countries/${country}`
+  }
+
+    try {
+        const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(changeableURL);
+
+        return { confirmed, recovered, deaths, lastUpdate };
+    } catch(error) {
+      console.log(error)
+    }
+}
+
+export const fetchDailyData = async () => {
+    try {
+      const { data } = await axios.get('https://api.covidtracking.com/v1/us/daily.json');
+  
+      return data.map(({ positive, death, dateChecked: date }) => ({ confirmed: positive, deaths: death, date }));
+    } catch (error) {
+      return error;
+    }
+  };
+
+  export const fetchCountries = async () => {
+    try {
+      const { data: { countries }} = await axios.get(`${url}/countries`);
+
+      return countries.map((country) => country.name);
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
